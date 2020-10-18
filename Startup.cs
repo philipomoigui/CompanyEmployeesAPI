66887cf93994e09d4b147f54adc1c01c04306ad7
philip omoigui <philipomoigui@gmail.com>
2020-10-18 03:26:30 +0100

    Content Negotiation for csv format

--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,9 @@ namespace CompanyEmployees
             {
                 config.RespectBrowserAcceptHeader = true;
                 config.ReturnHttpNotAcceptable = true;
-            }).AddXmlDataContractSerializerFormatters();
+            })
+             .AddXmlDataContractSerializerFormatters()
+             .AddCustomCsvFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
